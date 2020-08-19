39105bcb772c6defa234c609bb3da37a39c17955
Dave Barner <dave.barner@rocketinsights.com>
2020-08-18 22:03:21 -0400

    more

--- src/content/draftbits.js
+++ src/content/draftbits.js
@@ -5,5 +5,6 @@ export const draftBits = [
   'The failure in focusing on meditation as the end-all-be-all',
   'My initial launch into yoga',
   'In favor of the desktop junk folder',
+  'In defense of the nose job',
   "I had a chat tonight with a friend who's spent more of their life doing yoga than I have.  I'm raising this not to highlight a competitiaton, but moreso that her insight feels important in my journey because of her experience.  I asked her what I should be feeling, what I should be searching for, where I should be trying to metaphorically reach when practicing.  What she previously reached for.  I kind of knew the answer.  That there wasn't any pursuit.  But I stubbornly wanted a pursuit.  Why spend time practicing if I never were to gain.  In discussion, I realized the gain was much like meditation - there is always only one should -  the should to be present. To feel. To acknowledge.  That itch on my foot.  The outer bounds of my flexibility.  My inner dialogue that laughs at the my gumby-shaped body.  The thought that judges that self laughter and wonders if it's self shame, while still chuckling at my inner voice challenging itself.  Bingo, pursuit.  Hello, self." 
 ]
